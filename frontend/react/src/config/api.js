70a1052f0c3652aa4cda876bf5c4dce4cafd542f
renya <rsmilek5492@gmail.com>
2025-01-01 18:37:29 +0900

    Obfuscate LAMBDA_URL in API configuration for security

--- frontend/react/src/config/api.js
+++ frontend/react/src/config/api.js
@@ -1,6 +1,6 @@
 // API設定
 export const API_CONFIG = {
-    LAMBDA_URL: "https://4x6npooh2hqkdfvhzayo7wqpsm0gnnim.lambda-url.ap-northeast-1.on.aws/",
+    LAMBDA_URL: "https://XXXXXXXXXXXXXXXXXXXXXXXXXXXXX.lambda-url.ap-northeast-1.on.aws/",
     ENDPOINT: "api/stream_graph",
     get API_URL() {
       return `${this.LAMBDA_URL}${this.ENDPOINT}`;
